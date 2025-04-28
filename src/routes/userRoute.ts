942a45e68394c23afdcf0ea4af01e96eb5541e28
BenZeta <benpardede3@gmail.com>
2025-04-28 20:13:24 +0700

    fix: remove double middleware on userroute

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,53 +1,25 @@
 import { PERMISSION_ACTION } from '@prisma/client';
 import express from 'express';
 import userController from '../controllers/userController';
-import { authenticateToken } from '../middlewares/authentication';
 import { checkPermission } from '../middlewares/permission';
 
 const router = express.Router();
 
 // Protected routes with permission checks
-router.get(
-  '/',
-  authenticateToken,
-  checkPermission('user', PERMISSION_ACTION.READ),
-  userController.getAllUsers,
-);
+router.get('/', checkPermission('user', PERMISSION_ACTION.READ), userController.getAllUsers);
 router.get(
   '/archived',
-  authenticateToken,
   checkPermission('user', PERMISSION_ACTION.READ),
   userController.getArchivedUsers,
 );
-router.get(
-  '/:id',
-  authenticateToken,
-  checkPermission('user', PERMISSION_ACTION.READ),
-  userController.getUserById,
-);
-router.post(
-  '/',
-  authenticateToken,
-  checkPermission('user', PERMISSION_ACTION.CREATE),
-  userController.createUser,
-);
-router.put(
-  '/:id',
-  authenticateToken,
-  checkPermission('user', PERMISSION_ACTION.UPDATE),
-  userController.updateUser,
-);
+router.get('/:id', checkPermission('user', PERMISSION_ACTION.READ), userController.getUserById);
+router.post('/', checkPermission('user', PERMISSION_ACTION.CREATE), userController.createUser);
+router.put('/:id', checkPermission('user', PERMISSION_ACTION.UPDATE), userController.updateUser);
 router.patch(
   '/:id/unarchived',
-  authenticateToken,
   checkPermission('user', PERMISSION_ACTION.UPDATE),
   userController.unarchiveUser,
 );
-router.delete(
-  '/:id',
-  authenticateToken,
-  checkPermission('user', PERMISSION_ACTION.DELETE),
-  userController.deleteUser,
-);
+router.delete('/:id', checkPermission('user', PERMISSION_ACTION.DELETE), userController.deleteUser);
 
 export default router;
