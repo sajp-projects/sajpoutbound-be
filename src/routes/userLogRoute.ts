579404d46c22e6121939e84af4915a64b7fcb641
BenZeta <benpardede3@gmail.com>
2025-05-01 23:25:03 +0700

    fix: userlogs route naming

--- src/routes/userLogRoute.ts
+++ src/routes/userLogRoute.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // Get logs for a specific user
 router.get(
-  '/user/:userId',
+  '/:userId',
   checkPermission('user', PERMISSION_ACTION.READ),
   userLogController.getUserLogs,
 );
