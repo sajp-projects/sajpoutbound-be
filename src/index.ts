340ca7ffede6fc2a99876156edfa1a5be97f6627
BenZeta <benpardede.hacktiv8@gmail.com>
2025-05-10 16:55:25 +0700

    fix: cors origin wrong domain

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,11 @@ const server = http.createServer(app);
 // Middleware
 app.use(
   cors({
-    origin: ['https://www.outmanage.vercel.app', 'http://localhost:5173'],
+    origin: [
+      'https://www.outmanage.vercel.app',
+      'https://outmanage.vercel.app',
+      'http://localhost:5173',
+    ],
     credentials: true,
   }),
 );
