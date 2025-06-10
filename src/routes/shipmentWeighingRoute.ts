de7e8364fb98a9ee139751177ce5dbc96f7bb17f
BenZeta <benpardede.hacktiv8@gmail.com>
2025-06-10 11:30:58 +0700

    fix: change bulk endpoint for weighing to /

--- src/routes/shipmentWeighingRoute.ts
+++ src/routes/shipmentWeighingRoute.ts
@@ -3,10 +3,7 @@ import shipmentController from '../controllers/shipmentController';
 
 const router = express.Router();
 
-// Weigh individual shipment item
-router.post('/', shipmentController.weighShipmentItem);
-
 // Bulk weigh all items for a product at once
-router.post('/bulk', shipmentController.bulkWeighShipmentItems);
+router.post('/', shipmentController.bulkWeighShipmentItems);
 
 export default router;
